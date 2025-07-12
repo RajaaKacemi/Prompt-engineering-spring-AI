66d528885bf9e861d51253e7b5589c806bb83449
RajaaKacemi <rajaa01.kacemi@gmail.com>
2025-07-12 18:19:06 +0100

    Add endpoint to extract info from image

---
+++ src/main/java/com/enset/promptengineeringspringai/controllers/MultiModalController.java
@@ -0,0 +1,35 @@
+package com.enset.promptengineeringspringai.controllers;
+
+import com.enset.promptengineeringspringai.outputs.CinModel;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MultiModalController {
+
+    private ChatClient chatClient;
+    @Value("classpath:/images/CIN.png")
+    private Resource image;
+
+    public MultiModalController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+
+    }
+
+    @GetMapping("/describe")
+    public CinModel describeImage(){
+        return chatClient.prompt()
+                .system("DOnne une description de l'image fournie")
+                .user(
+                        u->
+                                u.text("Decrire cette image")
+                                        .media(MediaType.IMAGE_PNG, image)
+                )
+                .call()
+                .entity(CinModel.class);
+    };
+}
---
+++ src/main/java/com/enset/promptengineeringspringai/outputs/CinModel.java
@@ -0,0 +1,10 @@
+package com.enset.promptengineeringspringai.outputs;
+
+public record CinModel(String nom,
+                       String prenom,
+                       String dateNaissance,
+                       String arabicFirstName,
+                       String arabicLastName,
+                       String dateValidate,
+                       String numero) {
+}
